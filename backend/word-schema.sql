8d2498acb80ec3b4ce9bbdcb1768680e85622df5
Quyen Pham <hoangquyen174@gmail.com>
2022-05-12 11:27:04 -0500

    update dataset

--- backend/word-schema.sql
+++ backend/word-schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE categories (
-    handle VARCHAR(25) PRIMARY KEY CHECK (handle = lower(handle)),
+    handle VARCHAR(50) PRIMARY KEY CHECK (handle = lower(handle)),
     name TEXT UNIQUE NOT NULL,
     description TEXT NOT NULL
 );
@@ -17,12 +17,12 @@ CREATE TABLE users (
 CREATE TABLE words (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    category_handle VARCHAR(25) NOT NULL    
+    category_handle VARCHAR(50) NOT NULL    
         REFERENCES categories ON DELETE CASCADE
 );
 
 CREATE TABLE views (
-    username VARCHAR(25)
+    username VARCHAR(50)
         REFERENCES users ON DELETE CASCADE,
     word_id INTEGER
         REFERENCES words ON DELETE CASCADE,
